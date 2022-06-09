845de858bbda6bbe9aa7526732a5739716c24287
Pedro Toselli <pedrotoselli@Pedros-MacBook-Air.local>
2022-06-08 22:04:36 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    document.querySelector('#dark-mode')
+      .addEventListener('change', () => {
+      document.querySelector('html').classList.toggle('darkmode');
+    });
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='div'>
+
+      <h1>Simple dark mode example</h1>
+
+      <form>
+        <input id="dark-mode" class="toggle" type="checkbox" name="Dark mode" role="switch" />
+        <label for="dark-mode" class="sr">Dark Mode</label>
+      </form>
     </div>
   );
 }
